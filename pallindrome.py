7b980e0db334964f11e3c303e4868828f38db891
Priyanka sharma <76725172+Priyankasharma181@users.noreply.github.com>
2021-11-20 18:14:47 +0530

    Add files via upload

---
+++ pallindrome.py
@@ -0,0 +1,22 @@
+# list1 = ["mom","madam","flower"]
+# i = 0
+# count = 0
+# while i< len(list1):
+#     m = list1[i][::-1]
+#     if m == list1[i]:
+#         count+=1
+#         print(m)
+#     i+=1
+# print(count)    
+
+
+a = "priy anka"
+b = 'sharma'
+c= '''riy
+ajk'''
+print(a)
+print(b)
+print(c)
+
+
+
